d124a94c04a6e519e9e7943e61a1c5d8997282a1
Will Johnston <will.johnston@kroger.com>
2023-11-08 11:24:55 -0800

    removes jhh-controller

--- apps/jhh-api/src/models/controllers/index.ts
+++ apps/jhh-api/src/models/controllers/index.ts
@@ -1,7 +1,3 @@
 import Express from "express";
 
 export interface IController {}
-export interface IJhhController extends IController {
-  express: () => Express.Application;
-  createApplication: () => Express.Application;
-}
